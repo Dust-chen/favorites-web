ddde6b9de04068a40017f1a273f4bbc799ee6007
周瑜 <zhaoqichen@fb12348.onaliyun.com>
2020-04-17 15:25:17 +0800

    提交代码

--- app/src/main/java/com/favorites/FavoritesApplication.java
+++ app/src/main/java/com/favorites/FavoritesApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
+
 @SpringBootApplication
 public class FavoritesApplication extends SpringBootServletInitializer {
 	
@@ -14,7 +22,28 @@ public class FavoritesApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(FavoritesApplication.class, args);
-    }
+//        SpringApplication.run(FavoritesApplication.class, args);
+        Map a = new ConcurrentHashMap<>();
+        Map b = new HashMap<String, String>();
+        b.put("a", "1");
+        b.put("b", "2");
+        // 1.
+        Iterator<Entry<String, String>> iterator = b.entrySet().iterator();
+        while (iterator.hasNext()){
+            Entry<String, String> next = iterator.next();
+            next.getKey();
+            next.getValue();
+        }
 
+        // 2.jdk1.8开始自带的遍历方法
+        b.forEach(new BiConsumer() {
+            @Override
+            public void accept(Object key, Object value) {
+                System.out.println(key + ":" + value);
+            }
+        });
+//        b.forEach((key, value) -> {
+//            System.out.println(key + ":" + value);
+//        });
+    }
 }
